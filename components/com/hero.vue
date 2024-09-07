8c15f87dabb45cfcde03593d0e9c8e15c47a2a82
陈明浩 <1210204146@zust.edu.cn>
2024-09-07 13:45:24 +0800

    update hero

---
+++ components/com/hero.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  descpition: string
+}>()
+</script>
+<template>
+  <div class="hero">
+    <h1>
+      {{ title }}
+    </h1>
+    <h2>
+      {{ descpition }}
+    </h2>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.hero {
+  @apply pb-2;
+  @apply flex flex-col;
+
+  h1 {
+    @apply text-4xl font-semibold;
+  }
+
+  h2 {
+    @apply mt-4;
+  }
+}
+</style>
